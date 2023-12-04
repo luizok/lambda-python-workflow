958a941fd64f670ebdc32c51ba0e6cbf750af5f8
Luiz Felipe <luizfelipesmaciel@gmail.com>
2023-12-04 18:54:20 -0300

    :recycle: Adapt source code

---
+++ app/lambda_function.py
@@ -0,0 +1,15 @@
+from src.foo import Foo
+
+
+def lambda_handler(payload, context):
+
+    print(f'Payload: {payload}')
+    print(f'Context: {context}')
+    foo = Foo()
+    res = foo.do_something(1, 2)
+
+    return res
+
+
+if __name__ == '__main__':
+    print(f'The result is {lambda_handler(dict(), None)}')
